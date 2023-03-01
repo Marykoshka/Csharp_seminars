51e22a30cd80be54616a23322002f90a11736a62
Marykoshka <bmw_999_05@mail.ru>
2023-03-01 21:58:34 +0300

    Seminar 1 on lesson

---
+++ seminar_1/project_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Квадрат числа
+
+Console.Write("Введите целое число: ");
+
+//Получение целого числа от пользователя
+int number = int.Parse(Console.ReadLine());
+
+//Квадрат входного числа
+int result = number * number;
+
+Console.WriteLine($"Квадрат входного числа = {result}");
+
---
+++ seminar_1/project_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+
+Console.WriteLine("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число В: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int numsqr = numberA * numberA;
+
+if(numsqr == numberB)
+{
+        Console.WriteLine("Число В - это квадрат числа А");
+}
+else
+{
+    Console.WriteLine("Число В - это не квадрат числа А");
+}
