6fd1f3a1922f6f84098ab8831633ecaf04b132eb
Marykoshka <bmw_999_05@mail.ru>
2023-03-22 22:29:42 +0300

    Домашка к семинару 2

---
+++ seminar_2/HW2/task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write($"Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+string lengthCheck = number.ToString();
+
+if (lengthCheck.Length == 3)
+{
+    int result = (number / 10) % 10;
+    Console.WriteLine($"Искомое число: {result}");
+}
+else
+{
+    Console.Write($"Неверный формат числа");
+}
---
+++ seminar_2/HW2/task13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+
+// ЛЕГКИЙ СПОСОБ ЧЕРЕЗ СТРОКИ:
+
+// string num = int.Parse(Console.ReadLine()).ToString();
+
+// if (num.Length >= 3)
+// {
+//     Console.Write($"Третья цифра числа: {num[2]}");
+// }
+// else
+// {
+//     Console.Write($"Третьей цифры нет или введен неверный формат числа");
+// }
+
+
+// ВТОРОЙ СПОСОБ:
+
+int num = int.Parse(Console.ReadLine());
+int temp = num;
+
+int count = 0;
+while (temp != 0)
+{
+    temp = temp / 10;
+    count++;
+}
+
+if (count >= 3)
+{
+    Console.WriteLine($"Введено {count}-значное число, третья цифра - {(num / (int)Math.Pow(10, count - 3)) % 10}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет или введен неверный формат числа");
+}
